19ce570f6617093746afe1be5a21b225f55b3da2
menghaocheng <menghaocheng@qq.com>
2020-05-31 22:59:16 +0800

    03.创建C++项目并导入OpenGL相关库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_ywl5320_nativeopengldemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++ Opengl";
     return env->NewStringUTF(hello.c_str());
 }
